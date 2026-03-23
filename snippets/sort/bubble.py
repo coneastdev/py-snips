164c3c2f8bec6a3f74ffc8b57f0290b0aeba356f
coneasdev <coneastdev@proton.me>
2026-03-23 13:34:59 +0000

    update bubble sort

--- snippets/sort/bubble.py
+++ snippets/sort/bubble.py
@@ -3,6 +3,7 @@
 # data sets
 number_dataset = [5, 3, 1, 6, 8, 5, 4, 3, 2, 1, 0, 5, 3, 7, 6]
 string_dataset = ["b", "l", "o", "k", "f", "r", "j"]
+float_dataset = [9.99, 2.6, 7.02, 5.92, 0.01, 4.0, -1.2]
 
 # algorithm
 def bubble_sort(dataset: list) -> list:
@@ -26,3 +27,4 @@ def bubble_sort(dataset: list) -> list:
 if __name__ == "__main__":
     print(bubble_sort(number_dataset))
     print(bubble_sort(string_dataset))
+    print(bubble_sort(float_dataset))
